518f6e66602cf0469344520e2a0282a17126486d
Avijit Biswas <avijitb440@gmail.com>
2020-10-05 11:34:26 +0530

    Initial commit with error handling

---
+++ controllers/bootcamps.js
@@ -0,0 +1,60 @@
+const ErrorResponse = require("../utils/errorResponse");
+const Bootcamp = require("../models/Bootcamp");
+const asyncHandler = require("../middleware/async");
+
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
+
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp)
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of   ${req.params.id}`, 404)
+    );
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
+
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
+
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!bootcamp)
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of   ${req.params.id}`, 404)
+    );
+
+  res.status(200).json({ success: true, data: bootcamp });
+});
+
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndRemove(req.params.id);
+
+  if (!bootcamp)
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of   ${req.params.id}`, 404)
+    );
+
+  res.status(200).json({ success: true, data: {} });
+});
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+dotenv.config({ path: "./config/config.env" });
+const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/bootcamps", bootcamps);
+
+app.use(errorHandler);
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
