83eddebd42a2252c309fe8aab5cfb4f6cb86b49a
Avijit Biswas <avijitb440@gmail.com>
2020-10-12 11:37:22 +0530

    Added geocoding feature and added seeder file

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
+
 const bootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -45,7 +47,7 @@ const bootcampSchema = new mongoose.Schema({
       type: [Number],
       index: "2dsphere",
     },
-    formatedAddress: String,
+    formattedAddress: String,
     street: String,
     city: String,
     state: String,
@@ -98,6 +100,23 @@ const bootcampSchema = new mongoose.Schema({
 
 bootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+bootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  this.address = undefined;
   next();
 });
 
